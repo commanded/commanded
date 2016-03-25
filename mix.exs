b97e015379fc8dddddfe2dbc7dccabe5d1c6bb21
Ben Smith <ben@10consulting.com>
2016-03-25 18:51:38 +0000

    Bump to v0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EventStore.Mixfile do
   def project do
     [
       app: :eventstore,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.2",
       description: description,
       package: package,
