efef2a9094087529b0fcd12af37fdd42d35b9a93
Stuart Corbishley <corbish@gmail.com>
2016-07-04 18:55:24 +0200

    Ability to change pool size and overflow.
    ```
    config :eventstore, EventStore.Storage,
      username: "postgres",
      password: "postgres",
      database: "eventstore_test",
      hostname: "localhost",
      pool_size: 5,
      max_overflow: 10
    ```

--- lib/event_store/storage/pool_supervisor.ex
+++ lib/event_store/storage/pool_supervisor.ex
@@ -5,25 +5,38 @@ defmodule EventStore.Storage.PoolSupervisor do
 
   @storage_pool_name :event_store_storage_pool
 
+  @defaults [
+    {:name, {:local, @storage_pool_name}},
+    {:worker_module, Postgrex},
+    {:size, 10},
+    {:max_overflow, 5}
+  ]
+
   def start_link do
     Supervisor.start_link(__MODULE__, nil)
   end
 
   def init(_) do
-    storage_pool_config = [
-      {:name, {:local, @storage_pool_name}},
-      {:worker_module, Postgrex},
-      {:size, 10},
-      {:max_overflow, 5}
-    ]
-
-    storage_config = Config.parse Application.get_env(:eventstore, EventStore.Storage)
+    config = Config.parse Application.get_env(:eventstore, EventStore.Storage)
 
     children = [
-      :poolboy.child_spec(@storage_pool_name, storage_pool_config, storage_config)
+      :poolboy.child_spec(@storage_pool_name, pool_opts(config), postgrex_opts(config))
     ]
 
     supervise(children, strategy: :one_for_one)
+  end
+
+  defp pool_opts(config) do
+    opts = [
+      size:         config[:pool_size],
+      max_overflow: config[:pool_max_overflow]
+    ] |> Enum.filter(fn {_,v} -> !is_nil(v) end)
+
+    Keyword.merge(@defaults, opts)
+  end
+
+  defp postgrex_opts(config) do
+    Keyword.take(config, [:username, :password, :database, :hostname, :port])
   end
 
 end
