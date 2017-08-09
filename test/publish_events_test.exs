372b9ea1e931dea737988d678fce09ec54cb0c40
Ben Smith <ben@10consulting.com>
2017-08-09 15:30:23 +0100

    Publisher must resume from latest `event_id` on restart

--- test/publish_events_test.exs
+++ test/publish_events_test.exs
@@ -2,7 +2,9 @@ defmodule EventStore.PublishEventsTest do
   use EventStore.StorageCase
 
   alias EventStore.{EventFactory,Publisher,Subscriptions,Subscriber,Wait}
+  alias EventStore.ProcessHelper
   alias EventStore.Subscriptions.Subscription
+  alias EventStore.Streams.Stream
 
   @all_stream "$all"
   @subscription_name "test_subscription"
@@ -16,12 +18,7 @@ defmodule EventStore.PublishEventsTest do
     stream2_events = EventFactory.create_recorded_events(1, stream2_uuid, 2)
     stream3_events = EventFactory.create_recorded_events(1, stream3_uuid, 3)
 
-    {:ok, subscriber} = Subscriber.start_link(self())
-    {:ok, subscription} = Subscriptions.subscribe_to_stream(@all_stream, @subscription_name, subscriber, start_from: :current)
-
-    Wait.until(fn ->
-      assert Registry.lookup(EventStore.Subscriptions.PubSub, @all_stream) !== []
-    end)
+    {:ok, subscriber, subscription} = subscribe_to_all_streams(start_from_event_id: 0)
 
     # notify stream2 events before stream3 and stream1 (out of order)
     Publisher.notify_events(stream2_uuid, stream2_events)
@@ -43,5 +40,44 @@ defmodule EventStore.PublishEventsTest do
     assert stream3_received_events == EventFactory.deserialize_events(stream3_events)
 
     assert Subscriber.received_events(subscriber) == EventFactory.deserialize_events(stream1_events ++ stream2_events ++ stream3_events)
+  end
+
+  test "should resume publishing on restart" do
+    stream1_uuid = UUID.uuid4()
+    stream1_events = EventFactory.create_events(1)
+
+    :ok = EventStore.append_to_stream(stream1_uuid, 0, stream1_events)
+
+    {:ok, subscriber, subscription} = subscribe_to_all_streams(start_from_event_id: 1)
+
+    restart_publisher()
+
+    stream2_uuid = UUID.uuid4()
+    stream2_events = EventFactory.create_recorded_events(1, stream2_uuid, 2)
+
+    Publisher.notify_events(stream2_uuid, stream2_events)
+
+    # should receive published events
+    assert_receive {:events, stream2_received_events}
+    assert stream2_received_events == EventFactory.deserialize_events(stream2_events)
+  end
+
+  defp subscribe_to_all_streams(opts) do
+    {:ok, subscriber} = Subscriber.start_link(self())
+    {:ok, subscription} = Subscriptions.subscribe_to_stream(@all_stream, @subscription_name, subscriber, opts)
+
+    Wait.until(fn ->
+      assert Registry.lookup(EventStore.Subscriptions.PubSub, @all_stream) !== []
+    end)
+
+    {:ok, subscriber, subscription}
+  end
+
+  defp restart_publisher do
+    ProcessHelper.shutdown(Publisher)
+
+    Wait.until(fn ->
+      assert Process.whereis(Publisher) != nil
+    end)
   end
 end
