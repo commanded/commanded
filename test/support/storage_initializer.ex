5f81b606462f4c32445dc8e3cc4d4d0220139d4d
Ben Smith <ben@10consulting.com>
2017-08-16 16:45:12 +0100

    Test storage initializer wait for distributed publishers

--- test/support/storage_initializer.ex
+++ test/support/storage_initializer.ex
@@ -1,16 +1,41 @@
 defmodule EventStore.StorageInitializer do
-  alias EventStore.Storage
+  use EventStore.Registration
+  import ExUnit.Assertions
+
+  alias EventStore.{Storage,Wait}
 
   def reset_storage! do
-    Application.stop(:eventstore)
-    Application.stop(:swarm)
+    before_reset(@registry)
 
-    {:ok, conn} = EventStore.configuration() |> EventStore.Config.parse() |> Postgrex.start_link()
+    with {:ok, conn} <- EventStore.configuration() |> EventStore.Config.parse() |> Postgrex.start_link() do
+      Storage.Initializer.reset!(conn)
+    end
 
-    Storage.Initializer.reset!(conn)
-
-    {:ok, _} = Application.ensure_all_started(:eventstore)
+    after_reset(@registry)
 
     :ok
+  end
+
+  defp before_reset(EventStore.Registration.Distributed) do
+    Application.stop(:swarm)
+    Application.stop(:eventstore)
+  end
+
+  defp before_reset(_registry) do
+    Application.stop(:eventstore)
+  end
+
+  # wait until the publisher processes have registered themselves
+  defp after_reset(EventStore.Registration.Distributed) do
+    {:ok, _} = Application.ensure_all_started(:swarm)
+    {:ok, _} = Application.ensure_all_started(:eventstore)
+
+    Wait.until(fn ->
+      assert length(@registry.members(EventStore.Publisher)) >= 2
+    end)
+  end
+
+  defp after_reset(_registry) do
+    {:ok, _} = Application.ensure_all_started(:eventstore)
   end
 end
