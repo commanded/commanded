f87a17f22ef7aa9084907c78db6d2883ffb07fff
Florian Ebeling <florian.ebeling@gmail.com>
2017-10-05 11:54:59 +0200

    Fail test if `event_type` isn't a known module
    `assert_receive_event` waits for an event of a specific type. But
    doesn't recognize if the given event type isn't valid, e.g. because it
    hasn't been alias or has a typo.
    
    This change ensures the type is a known one or else raises an
    assertion exception.

--- lib/commanded/assertions/event_assertions.ex
+++ lib/commanded/assertions/event_assertions.ex
@@ -50,6 +50,9 @@ defmodule Commanded.Assertions.EventAssertions do
   Assert that an event of the given event type, matching the predicate, is published. Verify that event using the assertion function.
   """
   def assert_receive_event(event_type, predicate_fn, assertion_fn) do
+    unless Code.ensure_compiled?(event_type) do
+      raise ExUnit.AssertionError, "event_type #{inspect(event_type)} not found"
+    end
     with_subscription(fn subscription ->
       do_assert_receive(subscription, event_type, predicate_fn, assertion_fn)
     end)
