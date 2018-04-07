0602a53e65629c1ce5c7e1ec191a817799e38d16
James Cheuk <jccf091@gmail.com>
2018-04-07 21:26:48 +0800

    add module support for to_string/1 to

--- lib/commanded/serialization/module_name_type_provider.ex
+++ lib/commanded/serialization/module_name_type_provider.ex
@@ -9,7 +9,9 @@ defmodule Commanded.Serialization.ModuleNameTypeProvider do
 
   @behaviour Commanded.EventStore.TypeProvider
 
-  def to_string(struct), do: Atom.to_string(struct.__struct__)
+  def to_string(struct) when is_map(struct), do: Atom.to_string(struct.__struct__)
+
+  def to_string(module), do: Atom.to_string(module)
 
   def to_struct(type) do
     type |> String.to_existing_atom() |> struct()
