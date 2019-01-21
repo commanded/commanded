a494204634d5f3dd97a75bcf7319a1e6da506e70
Ben Smith <ben@10consulting.com>
2019-01-21 19:21:27 +0000

    Derive `Jason.Encoder` protocol for `Commanded.EventStore.SnapshotData` struct

--- lib/commanded/event_store/snapshot_data.ex
+++ lib/commanded/event_store/snapshot_data.ex
@@ -4,20 +4,21 @@ defmodule Commanded.EventStore.SnapshotData do
   """
 
   @type t :: %Commanded.EventStore.SnapshotData{
-    source_uuid: String.t,
-    source_version: non_neg_integer,
-    source_type: String.t,
-    data: binary,
-    metadata: binary,
-    created_at: NaiveDateTime.t,
-  }
+          source_uuid: String.t(),
+          source_version: non_neg_integer,
+          source_type: String.t(),
+          data: binary,
+          metadata: binary,
+          created_at: NaiveDateTime.t()
+        }
 
+  @derive Jason.Encoder
   defstruct [
     :source_uuid,
     :source_version,
     :source_type,
     :data,
     :metadata,
-    :created_at,
+    :created_at
   ]
 end
