c21270d24d5d9d15734bc1c923996d83b3122e44
Dave Lucia <davelucianyc@gmail.com>
2020-12-08 18:26:14 -0500

    improve the tests

--- test/process_managers/process_manager_telemetry_test.exs
+++ test/process_managers/process_manager_telemetry_test.exs
@@ -3,23 +3,49 @@ defmodule Commanded.ProcessManagers.ProcessManagerTelemetryTest do
 
   import Mox
 
-  alias Commanded.Application.Config
-  alias Commanded.Application.Mock, as: MockApplication
-  alias Commanded.ExampleDomain.BankAccount.Commands.WithdrawMoney
-  alias Commanded.ExampleDomain.MoneyTransfer.Events.MoneyTransferRequested
-  alias Commanded.ExampleDomain.TransferMoneyProcessManager
-  alias Commanded.EventStore.Adapters.Mock, as: MockEventStore
-  alias Commanded.EventStore.SnapshotData
   alias Commanded.ProcessManagers.ProcessManagerInstance
+  alias Commanded.DefaultApp
 
   setup :set_mox_global
   setup :verify_on_exit!
 
+  defmodule Commands do
+    defmodule Ok do
+      defstruct [:message]
+    end
+  end
+
+  defmodule Event do
+    @derive Jason.Encoder
+    defstruct [:message, :type]
+  end
+
+  defmodule ExamplePM do
+    use Commanded.ProcessManagers.ProcessManager,
+      application: Commanded.DefaultApp,
+      name: __MODULE__
+
+    alias Commands.Ok
+
+    @derive Jason.Encoder
+    defstruct message: "init"
+
+    def handle(%ExamplePM{}, %Event{type: type, message: message}) do
+      case type do
+        "ok" -> %Ok{message: message}
+        "error" -> {:error, message}
+        "raise" -> raise message
+      end
+    end
+
+    def apply(%ExamplePM{}, %Event{message: message}),
+      do: %ExamplePM{message: message}
+  end
+
+  alias Commands.Ok
+
   setup do
-    Config.associate(self(), MockApplication,
-      application: MockApplication,
-      event_store: {MockEventStore, %{}}
-    )
+    start_supervised!(DefaultApp)
 
     attach_telemetry()
 
@@ -28,55 +54,26 @@ defmodule Commanded.ProcessManagers.ProcessManagerTelemetryTest do
 
   @handler "test-pm-handler"
 
-  # TODO change to a local example
-
   describe "process manager telemetry" do
     test "emit `[:commanded, :process_manager, :handle, :start]` event" do
-      transfer_uuid = UUID.uuid4()
-      debit_account = UUID.uuid4()
-      credit_account = UUID.uuid4()
-      expected_source_uuid = "\"TransferMoneyProcessManager\"-\"#{transfer_uuid}\""
+      uuid = UUID.uuid4()
 
-      expect(MockEventStore, :read_snapshot, fn _adapter_meta, ^expected_source_uuid ->
-        {:error, :snapshot_not_found}
-      end)
+      {:ok, instance} = start_process_manager_instance(uuid)
 
-      expect(MockEventStore, :record_snapshot, fn _adapter_meta, _snapshot -> :ok end)
-
-      expect(MockApplication, :dispatch, fn command, _opts ->
-        assert %WithdrawMoney{
-                 account_number: ^debit_account,
-                 transfer_uuid: ^transfer_uuid,
-                 amount: 100
-               } = command
-
-        :ok
-      end)
-
-      {:ok, instance} = start_process_manager_instance(transfer_uuid)
-
-      event =
-        to_recorded_event(%MoneyTransferRequested{
-          transfer_uuid: transfer_uuid,
-          debit_account: debit_account,
-          credit_account: credit_account,
-          amount: 100
-        })
+      event = to_recorded_event(%Event{message: "start", type: "ok"})
 
       :ok = ProcessManagerInstance.process_event(instance, event)
 
-      assert_receive({:"$gen_cast", _}, 1_000)
       assert_receive {[:commanded, :process_manager, :handle, :start], measurements, metadata}
-
       assert match?(%{system_time: _system_time}, measurements)
 
       assert match?(
                %{
-                 application: Commanded.Application.Mock,
-                 process_manager_module: Commanded.ExampleDomain.TransferMoneyProcessManager,
-                 process_manager_name: "TransferMoneyProcessManager",
-                 process_state: %Commanded.ExampleDomain.TransferMoneyProcessManager{},
-                 process_uuid: ^transfer_uuid,
+                 application: Commanded.DefaultApp,
+                 process_manager_module: ExamplePM,
+                 process_manager_name: "ExamplePM",
+                 process_state: %ExamplePM{message: "init"},
+                 process_uuid: ^uuid,
                  recorded_event: ^event
                },
                metadata
@@ -89,58 +86,30 @@ defmodule Commanded.ProcessManagers.ProcessManagerTelemetryTest do
     end
 
     test "emit `[:commanded, :process_manager, :handle, :stop]` event" do
-      transfer_uuid = UUID.uuid4()
-      debit_account = UUID.uuid4()
-      credit_account = UUID.uuid4()
-      expected_source_uuid = "\"TransferMoneyProcessManager\"-\"#{transfer_uuid}\""
+      uuid = UUID.uuid4()
 
-      expect(MockEventStore, :read_snapshot, fn _adapter_meta, ^expected_source_uuid ->
-        {:error, :snapshot_not_found}
-      end)
+      {:ok, instance} = start_process_manager_instance(uuid)
 
-      expect(MockEventStore, :record_snapshot, fn _adapter_meta, _snapshot -> :ok end)
-
-      expect(MockApplication, :dispatch, fn command, _opts ->
-        assert %WithdrawMoney{
-                 account_number: ^debit_account,
-                 transfer_uuid: ^transfer_uuid,
-                 amount: 100
-               } = command
-
-        :ok
-      end)
-
-      {:ok, instance} = start_process_manager_instance(transfer_uuid)
-
-      event =
-        to_recorded_event(%MoneyTransferRequested{
-          transfer_uuid: transfer_uuid,
-          debit_account: debit_account,
-          credit_account: credit_account,
-          amount: 100
-        })
+      event = to_recorded_event(%Event{message: "start", type: "ok"})
 
       :ok = ProcessManagerInstance.process_event(instance, event)
 
-      assert_receive({:"$gen_cast", _}, 1_000)
       assert_receive {[:commanded, :process_manager, :handle, :start], _measurements, _metadata}
 
       assert_receive {[:commanded, :process_manager, :handle, :stop], measurements, metadata}
 
-      assert match?(%{duration: _duration}, measurements)
+      assert match?(%{duration: _}, measurements)
       assert is_integer(measurements.duration)
 
       assert match?(
                %{
-                 application: Commanded.Application.Mock,
-                 process_manager_module: TransferMoneyProcessManager,
-                 process_manager_name: "TransferMoneyProcessManager",
-                 process_state: %TransferMoneyProcessManager{},
-                 process_uuid: ^transfer_uuid,
+                 application: Commanded.DefaultApp,
+                 process_manager_module: ExamplePM,
+                 process_manager_name: "ExamplePM",
+                 process_state: %ExamplePM{message: "init"},
+                 process_uuid: ^uuid,
                  recorded_event: ^event,
-                 commands: [
-                   %WithdrawMoney{}
-                 ]
+                 commands: [%Ok{message: "start"}]
                },
                metadata
              )
@@ -149,47 +118,72 @@ defmodule Commanded.ProcessManagers.ProcessManagerTelemetryTest do
                       _metadata}
     end
 
-    test "emit `[:commanded, :process_manager, :handle, :exception]` event" do
-      transfer_uuid = UUID.uuid4()
-      debit_account = UUID.uuid4()
-      credit_account = UUID.uuid4()
-      expected_source_uuid = "\"TransferMoneyProcessManager\"-\"#{transfer_uuid}\""
+    test "emit `[:commanded, :process_manager, :handle, :stop]` with error event" do
+      uuid = UUID.uuid4()
 
-      expect(MockEventStore, :read_snapshot, fn _adapter_meta, ^expected_source_uuid ->
-        {:error, :snapshot_not_found}
-      end)
+      {:ok, instance} = start_process_manager_instance(uuid)
 
-      expect(MockEventStore, :record_snapshot, fn _adapter_meta, _snapshot -> :ok end)
-
-      expect(MockApplication, :dispatch, fn command, _opts ->
-        assert %WithdrawMoney{
-                 account_number: ^debit_account,
-                 transfer_uuid: ^transfer_uuid,
-                 amount: 100
-               } = command
-
-        :ok
-      end)
-
-      {:ok, instance} = start_process_manager_instance(transfer_uuid)
-
-      event =
-        to_recorded_event(%MoneyTransferRequested{
-          transfer_uuid: transfer_uuid,
-          debit_account: debit_account,
-          credit_account: credit_account,
-          amount: 100
-        })
+      event = to_recorded_event(%Event{message: "stop", type: "error"})
 
       :ok = ProcessManagerInstance.process_event(instance, event)
 
-      assert_receive({:"$gen_cast", _}, 1_000)
+      assert_receive {[:commanded, :process_manager, :handle, :start], _measurements, _metadata}
+
+      assert_receive {[:commanded, :process_manager, :handle, :stop], measurements, metadata}
+
+      assert match?(%{duration: _}, measurements)
+      assert is_integer(measurements.duration)
+
+      assert match?(
+               %{
+                 application: Commanded.DefaultApp,
+                 process_manager_module: ExamplePM,
+                 process_manager_name: "ExamplePM",
+                 process_state: %ExamplePM{message: "init"},
+                 process_uuid: ^uuid,
+                 recorded_event: ^event,
+                 error: "stop"
+               },
+               metadata
+             )
+
+      refute_receive {[:commanded, :process_manager, :handle, :exception], _measurements,
+                      _metadata}
+    end
+
+    @tag capture_log: true
+    test "emit `[:commanded, :process_manager, :handle, :exception]` event" do
+      uuid = UUID.uuid4()
+
+      {:ok, instance} = start_process_manager_instance(uuid)
+
+      event = to_recorded_event(%Event{message: "exception", type: "raise"})
+
+      :ok = ProcessManagerInstance.process_event(instance, event)
+
       assert_receive {[:commanded, :process_manager, :handle, :start], _measurements, _metadata}
 
       refute_receive {[:commanded, :process_manager, :handle, :stop], _measurements, _metadata}
 
-      assert_receive {[:commanded, :process_manager, :handle, :exception], _measurements,
-                      _metadata}
+      assert_receive {[:commanded, :process_manager, :handle, :exception], measurements, metadata}
+
+      assert match?(%{duration: _}, measurements)
+      assert is_integer(measurements.duration)
+
+      assert match?(
+               %{
+                 application: Commanded.DefaultApp,
+                 process_manager_module: ExamplePM,
+                 process_manager_name: "ExamplePM",
+                 process_state: %ExamplePM{message: "init"},
+                 process_uuid: ^uuid,
+                 recorded_event: ^event,
+                 kind: :error,
+                 reason: %RuntimeError{message: "exception"},
+                 stacktrace: _
+               },
+               metadata
+             )
     end
   end
 
@@ -215,10 +209,10 @@ defmodule Commanded.ProcessManagers.ProcessManagerTelemetryTest do
   defp start_process_manager_instance(transfer_uuid) do
     start_supervised(
       {ProcessManagerInstance,
-       application: MockApplication,
+       application: DefaultApp,
        idle_timeout: :infinity,
-       process_manager_name: "TransferMoneyProcessManager",
-       process_manager_module: TransferMoneyProcessManager,
+       process_manager_name: "ExamplePM",
+       process_manager_module: ExamplePM,
        process_router: self(),
        process_uuid: transfer_uuid}
     )
