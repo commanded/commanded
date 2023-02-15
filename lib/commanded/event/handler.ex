cd95e43bfba752a400403a4da78a2b395fb558b3
dvic <info@dvic.io>
2023-02-15 15:18:19 +0100

    Mark initialize/1 as defoverridable

--- lib/commanded/event/handler.ex
+++ lib/commanded/event/handler.ex
@@ -614,7 +614,7 @@ defmodule Commanded.Event.Handler do
       @doc false
       def before_reset, do: :ok
 
-      defoverridable init: 0, init: 1, before_reset: 0
+      defoverridable init: 0, init: 1, initialize: 1, before_reset: 0
     end
   end
 
